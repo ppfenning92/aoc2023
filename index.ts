0e30a2e40ad121d10cf752b2ecd873d417e05029
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2023-12-01 10:27:57 +0100

    done

--- index.ts
+++ index.ts
@@ -2,22 +2,25 @@ import minimist from "minimist";
 
 const args = minimist(Bun.argv.slice(2), { string: ["day"] });
 
+const PATH = `./src/${args["day"]}`;
 const loadModule = async () => {
   try {
-    const path = `./src/${args["day"]}`;
-    console.log("loading module", path);
+    console.log("loading module", PATH);
 
-    return await import(path);
+    return await import(PATH);
   } catch (e) {
     console.error("Cannot load module.");
     throw e;
   }
 };
 
+const data = await Bun.file(`${PATH}/input.txt`).text();
 const { one, two } = await loadModule();
 
 console.log("Part 1");
 await one();
+await one(data);
 
 console.log("Part 2");
 await two();
+await two(data);
--- src/01/index.ts
+++ src/01/index.ts
@@ -1,6 +1,90 @@
-export const one = async () => {
-  console.log("from task ONE");
+export const one = async (data: string = EXAMPLE_ONE) => {
+  const lines = data.split("\n").filter(Boolean);
+
+  const numsInLines = lines
+    .map((l) => l.replaceAll(/[\D]/g, ""))
+    .map((digits) => `${digits.at(0)}${digits.at(-1)}`)
+    .map((s) => parseInt(s, 10));
+
+  // console.log(numsInLines);
+
+  const sum = numsInLines.reduce((sum, x) => (sum += x), 0);
+
+  console.log(sum);
 };
-export const two = async () => {
-  console.log("from task TWO");
+export const two = async (data: string = EXAMPLE_TWO) => {
+  const lines = data.split("\n").filter(Boolean);
+
+  const numsInLines = lines
+    // .slice(8, 9)
+    .map((l) => {
+      const parsed = parser(l);
+      console.log(parsed, l, `${parsed.at(0)}${parsed.at(-1)}`);
+      return parsed;
+    })
+    // .map((l) => l.replaceAll(/[\D]/g, ""));
+    .map((digits) => `${digits.at(0)}${digits.at(-1)}`)
+    .map((s) => parseInt(s, 10));
+
+  // console.log(numsInLines);
+
+  const sum = numsInLines.reduce((sum, x) => (sum += x), 0);
+
+  console.log(sum);
 };
+
+const EXAMPLE_ONE = `
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+`;
+const EXAMPLE_TWO = `
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+`;
+
+const words = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+] as const;
+
+const parser = (s: string, parsed: string[] = []): string[] => {
+  const match = words.find((w) => s.startsWith(w));
+
+  if (s.length === 0 || !s) {
+    return parsed;
+  }
+  if (match) {
+    return parser(s.slice(1), [...parsed, parseMap[match]]);
+  }
+
+  if (s.match(/^\d/)) {
+    return parser(s.slice(1), [...parsed, s.at(0) as string]);
+  }
+
+  return parser(s.slice(1), parsed);
+};
+const parseMap = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+} as const;
