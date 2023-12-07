4b13cde6167a6db567f3a61865c5df06e95d77be
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2023-12-07 19:26:37 +0100

    day 5 parallel

--- src/05/index.ts
+++ src/05/index.ts
@@ -1,7 +1,9 @@
-import { sumReducer, toInt } from '../utils';
+import { toInt } from '../utils';
 import { prepare } from '../utils/fetch-challenge';
 import { Res } from '../utils/types';
+import { Worker } from 'node:worker_threads';
 
+import { MultiBar, Presets, SingleBar } from 'cli-progress';
 const EX1_RES = 35;
 const EX1_DAT = `
 seeds: 79 14 55 13
@@ -104,7 +106,7 @@ humidity-to-location map:
 //     return [seeds, maps];
 // };
 
-const parser = (
+export const parser = (
     data: string
 ): [number[], Record<string, Array<{ target: number; source: number; range: number }>>] => {
     const [seedString, ...mapStrings] = data.trim().split('\n\n');
@@ -130,7 +132,7 @@ const parser = (
     return [seeds.map(toInt), maps];
 };
 
-const ORDER = [
+export const ORDER = [
     'seed-to-soil',
     'soil-to-fertilizer',
     'fertilizer-to-water',
@@ -175,8 +177,8 @@ const one = async (data: string): Promise<Res> => {
 const EX2_RES = '46';
 const EX2_DAT = EX1_DAT;
 
-const two = async (data: string): Promise<Res> => {
-    let [seedRanges, maps] = parser(data);
+const two_ = async (data: string): Promise<Res> => {
+    const [seedRanges, maps] = parser(data);
 
     let currentLowestLocation = Number.MAX_SAFE_INTEGER;
     let total = 0;
@@ -214,6 +216,66 @@ const two = async (data: string): Promise<Res> => {
     return currentLowestLocation;
 };
 
+const two = async (data: string): Promise<Res> => {
+    const [seedRanges] = parser(data);
+    const threads = new Set<Worker>();
+    const responses: number[] = [];
+    const progress = new MultiBar(
+        {
+            clearOnComplete: false,
+            hideCursor: true,
+            format: ' {bar} | {i} | {value}/{total}  || ETA: {eta_my}s',
+        },
+        Presets.shades_grey
+    );
+    const progressBars: Record<string, SingleBar> = {};
+    const res: number[] = await new Promise((resolve) => {
+        for (let i = 0; i < seedRanges.length; i += 2) {
+            const [start, end] = seedRanges.slice(i, i + 2);
+            progressBars[i] = progress.create(100, 0, { i });
+
+            const worker = new Worker(new URL('./worker.ts', import.meta.url).href, {
+                workerData: { data, start, end, name: i },
+            });
+
+            threads.add(worker);
+        }
+
+        for (const worker of threads) {
+            worker.on('error', (err) => {
+                throw err;
+            });
+            worker.on('exit', () => {
+                threads.delete(worker);
+                if (threads.size === 0) {
+                    resolve(responses);
+                }
+            });
+            worker.on(
+                'message',
+                (
+                    msg:
+                        | { prog: number; name: string | number; eta: number }
+                        | { done: number; name: string }
+                ) => {
+                    if ('prog' in msg) {
+                        progressBars[msg.name].update(msg.prog, { eta_my: msg.eta.toFixed() });
+                    }
+                    if ('done' in msg) {
+                        responses.push(msg.done);
+                        progressBars[msg.name].update(100, { eta: 'now' });
+                    }
+                }
+            );
+        }
+    });
+
+    progress.stop();
+    console.log('\n\n');
+
+    return Math.min(...res);
+};
+
 export const run = async (day: string) => {
     const data = await prepare(day);
 
@@ -225,6 +287,7 @@ export const run = async (day: string) => {
     const EX1_SOL = await one(EX1_DAT);
     if (EX1_SOL != EX1_RES) {
         const msg = `Part 1 failed!\nExpected: ${EX1_RES} - Received: ${EX1_SOL}`;
+
         console.error(msg);
         return;
     }
@@ -235,9 +298,7 @@ export const run = async (day: string) => {
         console.error('Part 2 not ready yet');
         return;
     }
-    const start = performance.now();
     const EX2_SOL = await two(EX2_DAT);
-    console.warn(performance.now() - start);
 
     if (EX2_SOL != EX2_RES) {
         const msg = `Part 2 failed!\nExpected: ${EX2_RES} - Received: ${EX2_SOL}`;
--- src/utils/perf.ts
+++
@@ -1,1 +0,0 @@
-export const perfLoop = (start: any) => {};
